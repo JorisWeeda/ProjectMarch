ca491643bd41e3dfc5d73bde7937e8d918a1f848
Bas Volkers <basvolkers99@gmail.com>
2020-10-21 14:08:20 +0200

    Use ugly method of cancelling old callbacks when a new callback is called

--- ros2/src/march_gain_scheduling/march_gain_scheduling/dynamic_pid_reconfigurer.py
+++ ros2/src/march_gain_scheduling/march_gain_scheduling/dynamic_pid_reconfigurer.py
@@ -1,16 +1,20 @@
 from threading import Event
 from typing import List
 
+import numpy as np
 import rclpy
 from march_shared_classes.exceptions.parameter_exceptions import ParameterGetException, ParameterSetException
 from march_shared_msgs.msg import CurrentGait, GainValues, GainValuesList
 from march_shared_msgs.srv import GetParamStringList, GetGainValuesList, SetGainValuesList
-from rclpy.callback_groups import ReentrantCallbackGroup
+from rclpy.callback_groups import ReentrantCallbackGroup, MutuallyExclusiveCallbackGroup, CallbackGroup
 from rclpy.node import Node
 from rclpy.task import Future
+from rclpy.duration import Duration
 
 from .one_step_linear_interpolation import interpolate
+from .interpolation_errors import NegativeValueError, UnequalLengthError
 
+GAINS = ['p', 'i', 'd']
 
 class DynamicPIDReconfigurer(Node):
     def __init__(self):
@@ -18,15 +22,16 @@ class DynamicPIDReconfigurer(Node):
         super().__init__('gain_scheduling_node', automatically_declare_parameters_from_overrides=True)
         self._gait_type = None
         self._joint_list = self.init_joint_list()
-        self._last_update_times = []          
-        self.current_gains = []
+        self._last_update_time = None
+        self.current_gains = np.zeros((len(self._joint_list), len(GAINS)))
         self._linearize = self.get_parameter('linearize_gain_scheduling').get_parameter_value().bool_value
         self._gradient = self.get_parameter('linear_slope').get_parameter_value().double_value
 
         self.callback_group = ReentrantCallbackGroup()
+        # self.callback_group = MutuallyExclusiveCallbackGroup()
         self.create_subscription(msg_type=CurrentGait, topic='/march/gait_selection/current_gait',
                                  callback=self.gait_selection_callback, callback_group=self.callback_group,
-                                 qos_profile=10)
+                                 qos_profile=1)
 
         # Getter and setter clients to interact with the ROS1 parameter server
         self.gains_getter_client = self.create_client(GetGainValuesList, 'march/parameter_server/get_gain_values_list')
@@ -34,6 +39,8 @@ class DynamicPIDReconfigurer(Node):
 
         self.get_current_gains_event = Event()
         self.set_current_gains_event = Event()
+
+        self.callback_num = 0
 
     def init_joint_list(self) -> List[str]:
         """
@@ -51,6 +58,7 @@ class DynamicPIDReconfigurer(Node):
 
     def gait_selection_callback(self, msg):
         self.get_logger().info('---Gait callback: ' + str(msg.gait_type) + '---')
+        self.get_logger().info(str(self.callback_group))
 
         new_gait_type = msg.gait_type
         if new_gait_type is None or new_gait_type == '' \
@@ -64,36 +72,48 @@ class DynamicPIDReconfigurer(Node):
         self.get_current_gains()
         needed_gains = self.get_needed_gains()
 
-        if not self.is_interpolation_done(needed_gains):
-            self.perform_interpolation(needed_gains)
+        self.callback_num += 1
+        curr_callback_num = self.callback_num
 
-    def perform_interpolation(self, needed_gains):
+        if not self.is_interpolation_done(needed_gains):
+            self.start_interpolation(needed_gains, curr_callback_num)
+
+    def start_interpolation(self, needed_gains: np.ndarray, curr_callback_num):
         rate = self.create_rate(10)
 
         self.get_logger().info(f'Beginning PID interpolation for gait type: {self._gait_type}')
         begin_time = self.get_clock().now()
-        self._last_update_times = len(self._joint_list) * [begin_time]
+        self._last_update_time = begin_time
         while not self.is_interpolation_done(needed_gains):
             self.client_update(needed_gains)
-            rate.sleep()
-        duration = round((self.get_clock().now() - begin_time).nanoseconds / 1e6)
-        self.get_logger().info(f'PID interpolation finished in {duration} ms')
 
-    def client_update(self, needed_gains: List[List[float]]):
+            if self.callback_num != curr_callback_num:
+                self.get_logger().info('New callback, closing this thread')
+                return
+
+            rate.sleep()
+        duration_ms = round((self.get_clock().now() - begin_time).nanoseconds / 1e6)
+        self.get_logger().info(f'PID interpolation finished in {duration_ms} ms')
+
+    def client_update(self, needed_gains: np.ndarray):
         """
         Perform interpolation if necessary and update the PID gains in the ROS1 parameter server.
 
         :param needed_gains The final gains to interpolate to
         """
         if self._linearize:
-            for i in range(len(self._joint_list)):
-                current_time = self.get_clock().now()
-                time_interval = current_time - self._last_update_times[i]
+            # for i in range(len(self._joint_list)):
+            current_time = self.get_clock().now()
 
-                self.current_gains[i] = interpolate(self.current_gains[i], needed_gains[i], self._gradient,
-                                                    time_interval)
+            # Convert time_interval to seconds
+            time_interval = (current_time - self._last_update_time).nanoseconds / 1e9
 
-                self._last_update_times[i] = self.get_clock().now()
+
+            self.get_logger().info(f'Calling interpolation() with {time_interval}:')
+            self.current_gains = interpolate(self.current_gains, needed_gains, self._gradient, time_interval)
+            # self.get_logger().info('Done interpolation()')
+
+            self._last_update_time = self.get_clock().now()
         else:
             self.current_gains = needed_gains
 
@@ -109,7 +129,7 @@ class DynamicPIDReconfigurer(Node):
         
         future = self.gains_setter_client.call_async(
             SetGainValuesList.Request(joints=self._joint_list, gain_values=GainValuesList(
-                gain_values_list=[GainValues(gains=gains) for gains in self.current_gains])))
+                gain_values_list=[GainValues(gains=list(gains)) for gains in self.current_gains])))
         future.add_done_callback(self.set_future_cb)
 
         self.set_current_gains_event.wait()
@@ -143,29 +163,41 @@ class DynamicPIDReconfigurer(Node):
         """
         result = future_done.result()
         if result.success:
-            self.current_gains = [list(gain_values.gains) for gain_values in result.gain_values.gain_values_list]
+            self.current_gains = np.array([gain_values.gains for gain_values in result.gain_values.gain_values_list])
         else:
             raise ParameterGetException(self._joint_list)
         self.get_current_gains_event.set()
 
-    def get_needed_gains(self) -> List[List[int]]:
+    def get_needed_gains(self) -> np.ndarray:
         """
         Pull the PID values from the the gains_per_gait_type.yaml config file
 
         :returns Returns a list, containing a list of pid gain values for each joint
         """
-        gains_list = []
-        for joint in self._joint_list:
+        gains_list = np.zeros((len(self._joint_list), len(GAINS)), dtype=float)
+        for index, joint in enumerate(self._joint_list):
             gains = self.get_parameters_by_prefix('gait_types.{gait_type}.{joint}'.
                                                   format(gait_type=self._gait_type, joint=joint))
-            gains_list.append([gain.get_parameter_value().integer_value for gain in gains.values()])
+            gains_list[index] = [gain.get_parameter_value().integer_value for gain in gains.values()]
         return gains_list
 
-    def is_interpolation_done(self, needed_gains: List) -> bool:
+    def is_interpolation_done(self, needed_gains: np.ndarray) -> bool:
         """
         Checks whether the current gains are equal to the needed gains for each joint
 
         :param needed_gains Gains that the current_gains list should be equal to if the interpolation is done
         :returns Returns True if the gains are equal, else False
         """
-        return self.current_gains == needed_gains
+        return (self.current_gains == needed_gains).all()
+
+
+class OverwritingCallbackGroup(CallbackGroup):
+    def can_execute(self, entity) -> bool:
+        pass
+
+    def beginning_execution(self, entity) -> bool:
+        pass
+
+    def ending_execution(self, entity) -> None:
+        pass
+
