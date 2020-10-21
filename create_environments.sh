a53988a2910a326fd96e9f580e954fd774517237
Thijs Raymakers <thijs@raymakers.nl>
2020-10-21 09:17:51 +0200

    Add installation of ROS 1 on Ubuntu Bionic

--- create_environments.sh
+++ create_environments.sh
@@ -79,10 +79,17 @@ check_error
 ################################
 
 print_info "Creating schroot profiles..."
-# Create ROS 1 and ROS 2 installation folders that will be shared across both ROS chroots
-sudo mkdir -p /srv/chroot/ros1/opt/ros/melodic
+# Create ROS 1 and ROS 2 chroot and installation folders that will be shared across
+# both ROS chroots
+ROS1_LOCATION="/srv/chroot/ros1"
+sudo mkdir -p $ROS1_LOCATION/opt/ros/melodic
 check_error
-sudo mkdir -p /srv/chroot/ros2/opt/ros/foxy
+sudo chmod -R 755 $ROS1_LOCATION
+check_error
+ROS2_LOCATION="/srv/chroot/ros2"
+sudo mkdir -p $ROS2_LOCATION/opt/ros/foxy
+check_error
+sudo chmod -R 755 $ROS2_LOCATION
 check_error
 
 # Create a schroot profile for ROS 1
@@ -131,7 +138,7 @@ tmpfs		/dev/shm	tmpfs	defaults	0	0
 /sys		/sys		none	rw,bind		0	0
 /tmp		/tmp		none	rw,bind		0	0
 $WORKSPACE_PATH	/home/$USERNAME/march	none	rw,bind	0	0
-/srv/chroot/ros2/opt/ros/foxy	/opt/ros/foxy	none	rw,bind	0	0
+$ROS2_LOCATION/opt/ros/foxy	/opt/ros/foxy	none	rw,bind	0	0
 EOF
 check_error
 
@@ -166,7 +173,7 @@ sudo tee <<EOF ros1.conf >/dev/null
 [ros1]
 description=Ubuntu 18.04 (Bionic) with ROS Melodic
 type=directory
-directory=/srv/chroot/ros1
+directory=$ROS1_LOCATION
 users=$USER
 root-groups=root
 profile=ros1
@@ -179,7 +186,7 @@ sudo tee <<EOF ros2.conf >/dev/null
 [ros2]
 description=Ubuntu 20.04 (Focal) with ROS 2 Foxy
 type=directory
-directory=/srv/chroot/ros2
+directory=$ROS2_LOCATION
 users=$USER
 root-groups=root
 profile=ros2
@@ -204,18 +211,72 @@ tee <<EOF /tmp/.start_ros1.sh >/dev/null
 xhost +local:
 schroot --automatic-session -c ros1
 EOF
+check_error
 
 chmod +x /tmp/.start_ros1.sh
+check_error
 cp /tmp/.start_ros1.sh /tmp/.start_ros2.sh
+check_error
 sed -i 's/ros1/ros2/g' /tmp/.start_ros2.sh
+check_error
 
-###########################################
-# CREATING UBUNTU DISTRIBUTIONS IN CHROOT #
-###########################################
+##############################################
+# DOWNLOADING UBUNTU DISTRIBUTIONS IN CHROOT #
+##############################################
+
+# Download the files for Ubuntu Bionic in the ROS 1 chroot
+#sudo debootstrap --variant=buildd --arch=amd64 bionic $ROS1_LOCATION http://archive.ubuntu.com/ubuntu/
+check_error
+# Download the files for Ubuntu Focal in the ROS 2 chroot
+#sudo debootstrap --variant=buildd --arch=amd64 focal $ROS2_LOCATION http://archive.ubuntu.com/ubuntu/
+check_error
+
+############################
+# INSTALLING UBUNTU BIONIC #
+############################
+
+# Define package locations
+sudo tee <<EOF $ROS1_LOCATION/etc/apt/sources.list >/dev/null
+deb http://archive.ubuntu.com/ubuntu bionic main
+deb http://archive.ubuntu.com/ubuntu bionic universe
+deb http://archive.ubuntu.com/ubuntu bionic restricted
+deb http://archive.ubuntu.com/ubuntu bionic multiverse
+deb http://packages.ros.org/ros/ubuntu bionic main
+EOF
+check_error
+
+# Add a non-root user with sudo privilege
+sudo schroot --automatic-session -c ros1 -- bash -c "if id -u $USERNAME; then useradd $USERNAME && usermod -a -G sudo; fi; chown -R $USERNAME:$USERNAME /home/$USERNAME"
+check_error
+
+# Install required packages
+sudo schroot --automatic-session -c ros1 -- bash -c "apt update && apt upgrade -y && apt install -y lsb-release sudo curl gpg"
+check_error
+
+# Add key from ROS 1 and install ROS melodic
+sudo schroot --automatic-session -c ros1 -- bash -c "apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654 && apt update && apt install ros-melodic-desktop-full"
+check_error
+
+# Add automatic sourcing to the .bashrc file of the user
+sudo schroot --automatic-session -c ros1 -- bash -c "echo 'source /opt/ros/melodic/setup.bash' > /home/$USERNAME/.bashrc"
+check_error
+
+# Install dependencies for building ROS 1 packages
+sudo schroot --automatic-session -c ros1 -- bash -c "apt install -y python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential python3-colcon-common-extensions"
+check_error
+sudo schroot --automatic-session -c ros1 -- bash -c "rosdep init"
+
+# Install March specific ROS 1 dependencies
+schroot --automatic-session -c ros1 -- bash -c "rosdep update"
+check_error
+schroot --automatic-session -c ros1 -- bash -c "rosdep install -y --from-paths /home/$USERNAME/march/ros1/src --ignore-src"
+check_error
 
 # Setup has been succesful, unhide the startup scripts
 mv /tmp/.start_ros1.sh start_ros1.sh
+check_error
 mv /tmp/.start_ros2.sh start_ros2.sh
+check_error
 
 print_info_bold "Installation succesful!"
 exit 0
