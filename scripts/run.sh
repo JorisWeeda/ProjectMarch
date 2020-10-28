f4730bf08c8f9fdafa04137dc25dbd61aa7d5e3f
Thijs Raymakers <thijs@raymakers.nl>
2020-10-28 11:21:00 +0100

    Move run.sh file to scripts folder

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,6 +1,7 @@
 # Ensure that X11 can be accessed from the chroot environment
-xhost +local;
+xhost +local
 
+# Start ROS 1, ROS 2 and the bridge
 setsid schroot -d "/home/$USERNAME" -c ros1 -- zsh -c "march_run_ros1"
 setsid schroot -d "/home/$USERNAME" -c ros2 -- zsh -c "march_run_ros2"
 setsid schroot -d "/home/$USERNAME" -c ros1 -- zsh -c "march_run_bridge"
