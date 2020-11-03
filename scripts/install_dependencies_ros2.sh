3364dd8fee2739523d55cced135a88edf6f09437
Thijs Raymakers <thijs@raymakers.nl>
2020-11-03 11:46:00 +0100

    Add control_msgs as a ROS 2 dependency

--- scripts/install_dependencies_ros2.sh
+++ scripts/install_dependencies_ros2.sh
@@ -18,8 +18,14 @@ function check_error ()
 
 print_info "Build March specific ROS 2 dependencies..."
 # Delete old folders
-schroot -d "/home/$USERNAME" -c ros -- zsh -c "cd /home/$USERNAME/march/.ros2_foxy/src/ros2 && rm xacro urdfdom_parser_py -rf"
+schroot -d "/home/$USERNAME" -c ros -- zsh -c "cd /home/$USERNAME/march/.ros2_foxy/src/ros2 && rm xacro urdfdom_parser_py control_msgs -rf"
 check_error
 # Clone the folders and build them
-schroot -d "/home/$USERNAME" -c ros -- zsh -c "cd /home/$USERNAME/march/.ros2_foxy/src/ros2 && git clone https://github.com/ros/xacro.git -b dashing-devel && git clone https://github.com/ros/urdf_parser_py.git -b ros2 && cd /home/$USERNAME/march/.ros2_foxy && colcon build --packages-select urdfdom_py xacro"
+schroot -d "/home/$USERNAME" -c ros -- zsh -c "
+cd /home/$USERNAME/march/.ros2_foxy/src/ros2 &&
+git clone https://github.com/ros/xacro.git -b dashing-devel &&
+git clone https://github.com/ros/urdf_parser_py.git -b ros2 &&
+git clone https://github.com/ros-controls/control_msgs.git -b foxy-devel &&
+cd /home/$USERNAME/march/.ros2_foxy &&
+colcon build --packages-select urdfdom_py xacro control_msgs"
 check_error
