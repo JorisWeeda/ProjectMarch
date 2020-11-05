75694e0b30644ee1c2dbb9a42ae7c05cce63c01c
Thijs Raymakers <thijs@raymakers.nl>
2020-11-05 15:33:38 +0100

    Add DISPLAY environmental variable to run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,8 @@
 xhost +local
 
 # Start ROS 1, ROS 2 and the bridge
-setsid gnome-terminal -- schroot -d "/home/$USERNAME" -c ros -- zsh -c "march_run_ros1"
-setsid gnome-terminal -- schroot -d "/home/$USERNAME" -c ros -- zsh -c "march_run_ros2"
-setsid gnome-terminal -- schroot -d "/home/$USERNAME" -c ros -- zsh -c "march_run_bridge"
+setsid gnome-terminal -- schroot -d "/home/$USERNAME" -c ros -- zsh -c "export DISPLAY=:0 && march_run_ros1"
+sleep 2
+setsid gnome-terminal -- schroot -d "/home/$USERNAME" -c ros -- zsh -c "export DISPLAY=:0 && march_run_bridge"
+sleep 2
+setsid gnome-terminal -- schroot -d "/home/$USERNAME" -c ros -- zsh -c "export DISPLAY=:0 && march_run_ros2"
