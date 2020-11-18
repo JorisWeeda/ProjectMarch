0eaf8f0b7edf75643ce675c6fe3250f0ce683b24
Bas Volkers <basvolkers99@gmail.com>
2020-11-18 15:59:51 +0100

    Add gzclient and gzserver launch

--- ros2/src/march_simulation/launch/gazebo_launch.launch.py
+++ ros2/src/march_simulation/launch/gazebo_launch.launch.py
@@ -7,10 +7,13 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
 
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
+    gazebo_ui = LaunchConfiguration('gazebo_ui')
+    robot_description = LaunchConfiguration('robot_description')
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -18,16 +21,28 @@ def generate_launch_description():
             default_value='true',
             description='Uses simulated time and publishes on /clock.'
         ),
+        DeclareLaunchArgument(
+            name='gazebo_ui',
+            default_value='true',
+            description='Launches the Gazebo UI.'
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(
+                get_package_share_directory('gazebo_ros'), 'launch',
+                'gzserver.launch.py')),
+            launch_arguments=[('world',
+                              PathJoinSubstitution([
+                                  get_package_share_directory('march_simulation'),
+                                  'worlds', 'march.world'
+                              ])),
+                              ('verbose', 'true'),
+                              ('use_sim_time', use_sim_time)]
+        ),
         IncludeLaunchDescription(PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('gazebo_ros'),
-                         'launch', 'gzserver.launch.py')),
-            launch_arguments=[('world',
-                               PathJoinSubstitution([
-                                   get_package_share_directory('march_simulation'),
-                                   'worlds', 'march.world'
-                               ])),
-                              ('verbose', 'true'),
-                              ('use_sim_time', use_sim_time),
-                              ('pause', 'false')]
-        )
+                         'launch', 'gzclient.launch.py')),
+            launch_arguments=[('use_sim_time', use_sim_time),
+                              ('verbose', 'true')],
+            condition=IfCondition(gazebo_ui)
+        ),
     ])
--- ros2/src/march_simulation/launch/march_world.launch.py
+++ ros2/src/march_simulation/launch/march_world.launch.py
@@ -16,18 +16,20 @@ def generate_launch_description():
     ground_gait = LaunchConfiguration('ground_gait')
     gazebo_ui = LaunchConfiguration('gazebo_ui')
 
-    xacro_path = PathJoinSubstitution(
-        [get_package_share_directory('march_description'),
-         'urdf',
-         robot])
+    xacro_path = os.path.join(get_package_share_directory('march_description'), 'urdf', 'march4.xacro')
+        # PathJoinSubstitution(
+        # [get_package_share_directory('march_description'),
+        #  'urdf',
+        #  robot])
     xacro_args = """k_velocity_value_hfe:=60.0 k_velocity_value_kfe:=60.0
     k_velocity_value_haa:=60.0 k_velocity_value_adpf:=15.0
     k_position_value_hfe:=5000.0 k_position_value_kfe:=5000.0
     k_position_value_haa:=5000.0 k_position_value_adpf:=5000.0
     max_effort_rotary:=200.0 max_effort_linear:=200.0
     ground_gait:="""
-    robot_description = Command(['xacro', ' ', xacro_path, '.xacro', ' ',
-                                 xacro_args, ground_gait])
+    robot_description = Command(['xacro', ' ', xacro_path])
+    #, ' ', xacro_args, ground_gait])
+
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -75,19 +77,19 @@ def generate_launch_description():
             default_value='effort_control',
             description='Changes the controller used by simulation.'
         ),
-        # Node(
-        #     package='robot_state_publisher',
-        #     executable='robot_state_publisher',
-        #     name='robot_state_publisher',
-        #     output='screen',
-        #     parameters=[{
-        #         'use_sim_time': use_sim_time,
-        #         'robot_description': robot_description
-        #     }]),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{
+                'use_sim_time': use_sim_time,
+                'robot_description': robot_description
+            }]),
         IncludeLaunchDescription(PythonLaunchDescriptionSource(
             os.path.join(get_package_share_directory('march_simulation'),
                          'launch', 'gazebo_launch.launch.py')),
-            launch_arguments=[('use_sim_time', use_sim_time)],
-            condition=IfCondition(gazebo_ui)
+            launch_arguments=[('use_sim_time', use_sim_time),
+                              ('gazebo_ui', gazebo_ui)]
         )
     ])
