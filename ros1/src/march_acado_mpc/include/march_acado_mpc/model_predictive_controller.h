c82519b8a83e52f1bacb291576bfcf55fa0cfa46
Maarten ten Voorde <m.tenvoorde1998@gmail.com>
2020-11-23 12:18:14 +0100

    created an empty joint effort controller

--- ros1/src/march_acado_mpc/include/march_acado_mpc/model_predictive_controller.h
+++ ros1/src/march_acado_mpc/include/march_acado_mpc/model_predictive_controller.h
@@ -8,32 +8,20 @@
 
 namespace model_predictive_controller {
 
-    class ModelPredictiveController: public controller_interface::Controller<hardware_interface::EffortJointInterface>
+    class ModelPredictiveController : public controller_interface::Controller<hardware_interface::EffortJointInterface>
     {
     public:
-        bool init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle &n)
-        {
-            // get joint name from the parameter server
-            std::string joint_name;
-            if (!n.getParam("Joint", joint_name))
-            {
-                ROS_ERROR("Could not find joint name");
-                return false;
-            }
-
-            // get the joint object to use in the realtime loop
-            joint_ = hw->getHandle(joint_name); // throws on failure
-            return true;
-        }
-
-        void update(const ros::Time& time, const ros::Duration& period) { }
-        void starting(const ros::Time& time) { }
-        void stopping(const ros::Time& time) { }
+        bool init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle &n);
+        void update(const ros::Time& time, const ros::Duration& period);
+        void starting(const ros::Time& time);
+        void stopping(const ros::Time& time);
 
     private:
         hardware_interface::JointHandle joint_;
+        const double gain_ = 30;
+        const double setpoint_ = 1.0;
     };
-    PLUGINLIB_DECLARE_CLASS(march_acado_mpc, ModelPredictiveController, model_predictive_controller::ModelPredictiveController, controller_interface::ControllerBase);
-} // namespace
+//    PLUGINLIB_DECLARE_CLASS(march_acado_mpc, ModelPredictiveController, model_predictive_controller::ModelPredictiveController, controller_interface::ControllerBase);
+} //model_predictive_controller
 
-#endif // MARCH_HARDWARE_MODEL_PREDICTIVE_CONTROLLER_H
+#endif //MARCH_HARDWARE_MODEL_PREDICTIVE_CONTROLLER_H
--- ros1/src/march_acado_mpc/src/model_predictive_controller.cpp
+++ ros1/src/march_acado_mpc/src/model_predictive_controller.cpp
@@ -1,5 +1,35 @@
-//
-// Created by mjmtv on 20-11-20.
-//
-
 #include "model_predictive_controller.h"
+#include <pluginlib/class_list_macros.hpp>
+
+namespace model_predictive_controller {
+
+    bool ModelPredictiveController::init(hardware_interface::EffortJointInterface* hw, ros::NodeHandle &n)
+    {
+        // get joint name from the parameter server
+        std::string joint;
+        if (!n.getParam("joint", joint)){
+            ROS_ERROR("Could not find joint name");
+            return false;
+        }
+
+        // get the joint object to use in the realtime loop
+        joint_ = hw->getHandle(joint);  // throws on failure
+        return true;
+    }
+
+    // Controller starting
+    void ModelPredictiveController::starting(const ros::Time& time) { }
+
+    // Controller running
+    void ModelPredictiveController::update(const ros::Time& time, const ros::Duration& period)
+    {
+        double error = setpoint_ - joint_.getPosition();
+        joint_.setCommand(gain_*error);
+    }
+
+    // Controller stopping
+    void ModelPredictiveController::stopping(const ros::Time& time) { }
+
+} //model_predictive_controller
+
+PLUGINLIB_EXPORT_CLASS(model_predictive_controller::ModelPredictiveController, controller_interface::ControllerBase);
