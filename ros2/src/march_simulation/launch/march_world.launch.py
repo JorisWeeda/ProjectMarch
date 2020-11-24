948af90aa5d6c1019f567ea5ade2b85da10a3a46
Bas Volkers <basvolkers99@gmail.com>
2020-11-24 12:45:18 +0100

    Refactor simulation launch file

--- ros2/src/march_simulation/launch/march_world.launch.py
+++ ros2/src/march_simulation/launch/march_world.launch.py
@@ -4,7 +4,7 @@ import os
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, PythonExpression
 from launch_ros.actions import Node
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.conditions import IfCondition, UnlessCondition
@@ -12,10 +12,14 @@ from launch.conditions import IfCondition, UnlessCondition
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
-    robot = LaunchConfiguration('robot')
-    ground_gait = LaunchConfiguration('ground_gait')
+    fake_sensor_data = LaunchConfiguration('fake_sensor_date')
     gazebo_ui = LaunchConfiguration('gazebo_ui')
+    debug = LaunchConfiguration('debug')
     fixed = LaunchConfiguration('fixed')
+    ground_gait = LaunchConfiguration('ground_gait')
+    obstacle = LaunchConfiguration('obstacle')
+    robot = LaunchConfiguration('robot')
+    controller = LaunchConfiguration('controller')
 
     xacro_path = PathJoinSubstitution([
         get_package_share_directory('march_description'), 'urdf', robot])
@@ -25,17 +29,20 @@ def generate_launch_description():
     # However, to limit the safety controller, we need to limit them.
     xacro_args = [" k_velocity_value_hfe:=", "60.0",
                   " k_velocity_value_kfe:=", "60.0",
-                  " k_velocity_value_haa:=", "60.0 ",
+                  " k_velocity_value_haa:=", "60.0",
                   " k_velocity_value_adpf:=", "15.0",
-                  " k_position_value_hfe:=", "5000.0 ",
+                  " k_position_value_hfe:=", "5000.0",
                   " k_position_value_kfe:=", "5000.0",
-                  " k_position_value_haa:=", "5000.0 ",
+                  " k_position_value_haa:=", "5000.0",
                   " k_position_value_adpf:=", "5000.0",
-                  " max_effort_rotary:=", "200.0 ",
+                  " max_effort_rotary:=", "200.0",
                   " max_effort_linear:=", "200.0",
                   " ground_gait:=", ground_gait]
     robot_description = Command(['xacro', ' ', xacro_path, '.xacro'] +
                                 xacro_args)
+
+    obstacle_not_none = IfCondition(PythonExpression(['"true" if "none" != "',
+                                                      obstacle, '" else "false"']))
 
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -92,6 +99,7 @@ def generate_launch_description():
                 'use_sim_time': use_sim_time,
                 'robot_description': robot_description
             }]),
+        # Launch world transformer if not fixed
         Node(
             package='march_simulation',
             executable='to_world_transform',
@@ -99,6 +107,7 @@ def generate_launch_description():
             output='screen',
             condition=UnlessCondition(fixed)
         ),
+        # Spawn march robot
         Node(
             package='gazebo_ros',
             executable='spawn_entity.py',
@@ -107,10 +116,35 @@ def generate_launch_description():
                        '-entity', 'march'],
             output='screen'
         ),
+        # Launch both gazebo client and server
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(os.path.join(
+                get_package_share_directory('gazebo_ros'), 'launch',
+                'gzserver.launch.py')),
+            launch_arguments=[('world',
+                               PathJoinSubstitution([
+                                   get_package_share_directory(
+                                       'march_simulation'),
+                                   'worlds', 'march.world'
+                               ])),
+                              ('verbose', 'true'),
+                              ('use_sim_time', use_sim_time),
+                              ('gdb', debug)]
+        ),
         IncludeLaunchDescription(PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('march_simulation'),
-                         'launch', 'gazebo_launch.launch.py')),
+            os.path.join(get_package_share_directory('gazebo_ros'),
+                         'launch', 'gzclient.launch.py')),
             launch_arguments=[('use_sim_time', use_sim_time),
-                              ('gazebo_ui', gazebo_ui)]
+                              ('verbose', 'true'),
+                              ('gdb', debug)],
+            condition=IfCondition(gazebo_ui)
+        ),
+        # Spawn obstacle
+        Node(
+            package='march_simulation',
+            executable='spawn_obstacle',
+            parameters=[{'obstacle': obstacle}],
+            output='screen',
+            condition=obstacle_not_none
         )
     ])
