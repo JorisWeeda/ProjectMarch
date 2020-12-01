7868cc96c259e1faf8db2d77749118a4beccd426
BasVolkers <basvolkers99@gmail.com>
2020-12-01 12:06:38 +0100

    Add subgait generator from branch 479

---
+++ ros2/src/gaits/march_testjoint_gait_generator/setup.py
@@ -0,0 +1,35 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'march_testjoint_generator'
+
+
+def data_files():
+    data = [
+        (os.path.join('share', 'ament_index', 'resource_index', 'packages'),
+         [os.path.join('resource', package_name)]),
+        (os.path.join('share', package_name), ['package.xml']),
+         (os.path.join('share', package_name, 'launch'),
+          glob('launch/*.launch.py'))
+    ]
+    return data
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=data_files(),
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Project March',
+    maintainer_email='software@projectmarch.nl',
+    description='Gait generator for the testjoint',
+    license='TODO: License declaration',
+    tests_require=['pytest', 'urfdom_py', 'unittest', 'parameterized'],
+    entry_points={
+        'console_scripts': [
+            f'generate_testjoint_gait = {package_name}.testjoint_gait_generator:main'
+        ],
+    },
+)
