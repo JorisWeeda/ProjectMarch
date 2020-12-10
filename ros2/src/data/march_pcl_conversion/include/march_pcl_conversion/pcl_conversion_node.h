bad8b22811c3cc4f6dac8f68a81bec0de6a9bcd5
Katja Schmahl <katjaschmahl@hotmail.com>
2020-12-10 14:33:07 +0100

    Implement working pcl node, still with errors on bridging depth images

--- ros2/src/data/march_pcl_conversion/include/march_pcl_conversion/pcl_conversion_node.h
+++ ros2/src/data/march_pcl_conversion/include/march_pcl_conversion/pcl_conversion_node.h
@@ -1,4 +1,17 @@
 #ifndef MARCH_GAZEBO_PLUGINS_PCL_CONVERSION_NODE_H
 #define MARCH_GAZEBO_PLUGINS_PCL_CONVERSION_NODE_H
 
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+
+class PclConversionNode : public rclcpp::Node
+{
+public:
+  PclConversionNode();
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription;
+
+private:
+  void pointcloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr);
+};
+
 #endif  // MARCH_GAZEBO_PLUGINS_PCL_CONVERSION_NODE_H
--- ros2/src/data/march_pcl_conversion/src/pcl_conversion_node.cpp
+++ ros2/src/data/march_pcl_conversion/src/pcl_conversion_node.cpp
@@ -2,26 +2,31 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include <sensor_msgs/msg/point_cloud2.hpp>
 using std::placeholders::_1;
 
-class PclConversionNode : public rclcpp::Node
-{
-public:
-  PclConversionNode() : Node("pcl_conversion_node")
-  {
-    std::string topic_name;
-    // Temporary solution to switch between connection with simulation or with physical camera
-    topic_name = "/camera/depth/color/points"; // Simulation
-//    topic_name = "/camera/pointcloud";  // Physical camera
-    subscription = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-        topic_name, 10, std::bind(&PclConversionNode::pointcloud_callback, this, _1)
-        );
-  }
 
-private:
-  void pointcloud_callback(const sensor_msgs::msg::PointCloud2) const
-  {
-    RCLCPP_INFO(this->get_logger(), "I heard something");
-  }
-};
+PclConversionNode::PclConversionNode() : Node("pcl_conversion_node")
+{
+  std::string topic_name;
+  // Temporary solution to switch between connection with simulation or with physical camera
+  topic_name = "/camera/depth/color/points"; // Simulation
+  topic_name = "/camera/pointcloud";  // Physical camera
+  subscription = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+      topic_name, 10, std::bind(&PclConversionNode::pointcloud_callback, this, _1)
+  );
+}
+
+void PclConversionNode::pointcloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+{
+  RCLCPP_INFO(this->get_logger(), "I heard something: '%s'", msg->header);
+
+}
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PclConversionNode>());
+  rclcpp::shutdown();
+  return 0;
+}
