55cacebc83d0ee5cdb2b64dfde8cd32ad017b2cf
Katja Schmahl <katjaschmahl@hotmail.com>
2020-12-14 11:37:54 +0100

    Working node, except for error caused by pcl conversions

--- ros2/src/data/march_pcl_conversion/include/march_pcl_conversion/pcl_conversion_node.h
+++ ros2/src/data/march_pcl_conversion/include/march_pcl_conversion/pcl_conversion_node.h
@@ -1,17 +1,19 @@
-#ifndef MARCH_GAZEBO_PLUGINS_PCL_CONVERSION_NODE_H
-#define MARCH_GAZEBO_PLUGINS_PCL_CONVERSION_NODE_H
-
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
+#include <pcl_conversions/pcl_conversions.h>
+
+#ifndef MARCH_PCL_CONVERSION_NODE_H
+#define MARCH_PCL_CONVERSION_NODE_H
 
 class PclConversionNode : public rclcpp::Node
 {
 public:
   PclConversionNode();
-  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription;
+  rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_;
 
 private:
   void pointcloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr);
 };
 
-#endif  // MARCH_GAZEBO_PLUGINS_PCL_CONVERSION_NODE_H
+#endif  // MARCH_PCL_CONVERSION_NODE_H
+//
--- ros2/src/data/march_pcl_conversion/src/pcl_conversion_node.cpp
+++ ros2/src/data/march_pcl_conversion/src/pcl_conversion_node.cpp
@@ -1,26 +1,23 @@
-#include "../include/march_pcl_conversion/pcl_conversion_node.h"
+#include <march_pcl_conversion/pcl_conversion_node.h>
 #include <memory>
-
+#include <functional>
 #include "rclcpp/rclcpp.hpp"
-#include <sensor_msgs/msg/point_cloud2.hpp>
-using std::placeholders::_1;
+#include "sensor_msgs/msg/point_cloud2.hpp"
 
-
-PclConversionNode::PclConversionNode() : Node("pcl_conversion_node")
+PclConversionNode::PclConversionNode() : rclcpp::Node::Node("march_pcl_conversion_node")
 {
   std::string topic_name;
   // Temporary solution to switch between connection with simulation or with physical camera
   topic_name = "/camera/depth/color/points"; // Simulation
   topic_name = "/camera/pointcloud";  // Physical camera
-  subscription = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-      topic_name, 10, std::bind(&PclConversionNode::pointcloud_callback, this, _1)
+  subscription_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+      "test", 10, std::bind(&PclConversionNode::pointcloud_callback, this, std::placeholders::_1)
   );
 }
 
-void PclConversionNode::pointcloud_callback(const sensor_msgs::msg::PointCloud2::SharedPtr msg)
+void PclConversionNode::pointcloud_callback(sensor_msgs::msg::PointCloud2::SharedPtr msg)
 {
   RCLCPP_INFO(this->get_logger(), "I heard something: '%s'", msg->header);
-
 }
 
 int main(int argc, char * argv[])
